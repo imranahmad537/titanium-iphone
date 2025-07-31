9ecb0673e03df185148b7b008f5a2406640f0f29
imranahmad537 <ahmadimrankh99@gmail.com>
2025-07-31 10:44:19 +0500

    Committed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
         <nav className='flex w-full screen-max-width'>
             <img src={appleImg} alt="Apple" width={14} height={18}/>
 
-            <div className='flex flex-1 justify-center max-sm:hidden'>
-                 {navLists.map((nav, item) => (
-                    <div key={item} className="px-5 text-sm cursor-pointer text-[gray] hover:text-white transition-all">
+            <div className='flex flex-1 justify-center max-sm:hidden  '>
+                 {navLists.map((nav, index) => (
+                    <div key={nav} className="px-5  text-sm cursor-pointer text-[gray] hover:text-white transition-all">
                         {nav}
                     </div>
                  ))}
